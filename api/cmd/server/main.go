e9ae8a74b499ca3dd096b74c6cbf9b9dd7ebaad3
Shinya Kinjo <shinya.work0720@gmail.com>
2023-07-11 09:55:09 +1200

    :building_construction: Simplify server.go

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"context"
+	"net/http"
 
-	"github.com/mokoshin0720/monorepo/api/server"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	server, sclnup, err := server.New()
-	if err != nil {
-		panic(err)
-	}
-	defer sclnup()
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	ctx := context.Background()
-	server.Run(ctx)
+	log.Info().Msg("Starting server...")
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	log.Info().Msg("routing server...")
+
+	http.ListenAndServe(":9000", r)
 }
