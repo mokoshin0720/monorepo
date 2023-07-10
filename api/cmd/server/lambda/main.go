3916d0b54ac20b391e9ae9a8934f35f52ca6e483
Shinya Kinjo <shinya.work0720@gmail.com>
2023-07-11 10:22:34 +1200

    :loud_sound: Add lambda logs

--- api/cmd/server/lambda/main.go
+++ api/cmd/server/lambda/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/mokoshin0720/monorepo/api/cmd/server"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	log.Info().Msg("Starting lambda server...")
 	lambda.Start(server.HandleRequest)
 }
