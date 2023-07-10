7469b88a94d94f5a45f3668df77b881bc511c555
Shinya Kinjo <shinya.work0720@gmail.com>
2023-07-11 11:41:36 +1200

    :hammer: Router in init method

--- api/cmd/server/lambda/main.go
+++ api/cmd/server/lambda/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	chiadapter "github.com/awslabs/aws-lambda-go-api-proxy/chi"
-	"github.com/mokoshin0720/monorepo/api/cmd/server"
+	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog/log"
 )
 
@@ -15,9 +16,16 @@ var chiLambda *chiadapter.ChiLambda
 func init() {
 	log.Info().Msg("Initializing lambda server...")
 
-	router := server.HandleRequest()
+	r := chi.NewRouter()
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
 
-	chiLambda = chiadapter.New(router)
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	chiLambda = chiadapter.New(r)
 }
 
 func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
